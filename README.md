b191bcf6dffaaa3aca5bfd9aee1d37479d5870d2
YasminMattos <yasmin.mattos.conislla@gmail.com>
2017-12-31 20:03:23 -0500

    añade splash, filtro general, mouseover y modals

---
+++ README.md
@@ -0,0 +1,10 @@
+# **Foodmap**
+--------
+
+- ## **Objetivo**
+
+    Crear una web-app que muestre los restaurantes que se encuentren cerca.
+
+- ## **Mobile view**
+
+    ![](assets/images/view.png)
---
+++ js/data.js
@@ -0,0 +1,76 @@
+// restaurants database
+var data = {
+  'restaurants': [
+    {
+      'name': 'King-Kong',
+      'keywords': ['mexicana', 'tacos', 'pozole', 'latina', 'quesadillas'],
+      'image': 'https://cdn.forbes.com.mx/2015/07/Foto-Cortes%C3%ADa-La-Buena-Barra.jpg',
+      'radius': 5000,
+      'average-cost': 55,
+    },
+    {
+      'name': 'Red Meat',
+      'keywords': ['carnes', 'lomo', 'pollería', 'parrillas'],
+      'image': 'http://www.hauteliving.com/wp-content/uploads/2012/03/Gary-Danko.jpg',
+      'radius': 3000,
+      'average-cost': 35,
+    },
+    {
+      'name': 'Buon appetito',
+      'keywords': ['tartas', 'pizzas', 'pastas', 'lasagna'],
+      'image': 'http://cdmxtravel.com/assets/cache/d0866fb7fef7340334755089f89bdfeb/restaurant-1003334-the-comrade-6_849x477_adaptiveResize.jpg',
+      'radius': 4200,
+      'average-cost': 87,
+    },
+    {
+      'name': 'Encanto Azul',
+      'keywords': ['marino', 'ceviche', 'criollo'],
+      'image': 'https://media.cntraveler.com/photos/53da9ea3dcd5888e145c0e71/master/w_775,c_limit/ithaa-underwater-restaurant-conrad-maldives.jpg',
+      'radius': 2500,
+      'average-cost': 120,
+    },
+    {
+      'name': 'La Maroma',
+      'keywords': ['mexicana', 'tacos', 'pozole', 'latino'],
+      'image': 'http://bi.gazeta.pl/im/37/84/13/z20466743V,Wnetrza-Restauracji-Radosc.jpg',
+      'radius': 1000,
+      'average-cost': 25,
+    },
+    {
+      'name': 'Basta ya!',
+      'keywords': ['criollo', 'latino'],
+      'image': 'http://guiadeltrabajo.com/wp-content/uploads/2017/02/Harry-Sasson-Bar-e1486218926388.jpg',
+      'radius': 1800,
+      'average-cost': 28,
+    },
+    {
+      'name': 'El Sevillano',
+      'keywords': ['español', 'gazpacho', 'paella', 'calamares'],
+      'image': 'http://guiadeltrabajo.com/wp-content/uploads/2017/02/Harry-Sasson-Bar-e1486218926388.jpg',
+      'radius': 5600,
+      'average-cost': 50,
+    },
+    {
+      'name': 'The Galaxy',
+      'keywords': ['ensaladas', 'frutas', 'jugos', 'espacial'],
+      'image': 'http://www.popcidade.com.br/wp-content/uploads/2015/06/jedi-2-960x600.jpg',
+      'radius': 2300,
+      'average-cost': 30,
+    },
+    {
+      'name': 'La buena barra',
+      'keywords': ['mexicana', 'tacos', 'pozole', 'latino'],
+      'image': 'https://cmspro.h10hotels.com/ImagenesRestaurantes/hpc_1674083394.jpg',
+      'radius': 23000,
+      'average-cost': 85,
+    },
+    {
+      'name': 'Pasteles verdes',
+      'keywords': ['pasteles', 'variado', 'tradicional', 'latino'],
+      'image': 'http://cdn.vogue.mx/uploads/images/thumbs/mx/vog/2/s/2017/20/las_mejores_terrazas_en_la_ciudad_de_mexico_911109299_620x.jpg',
+      'radius': 500,
+      'average-cost': 30,
+    },
+
+  ]
+}
